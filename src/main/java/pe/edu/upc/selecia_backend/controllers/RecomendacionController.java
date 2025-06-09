ec8e982eef99cadc4ef796c61af65d8fa65cd20e
Daniel <danielhuertaarellanos.1.12@gmail.com>
2025-06-09 14:25:18 -0500

    avance

--- src/main/java/pe/edu/upc/selecia_backend/controllers/RecomendacionController.java
+++ src/main/java/pe/edu/upc/selecia_backend/controllers/RecomendacionController.java
@@ -112,7 +112,7 @@ public class RecomendacionController {
     }*/
 
     @GetMapping("/rank/oferta/{idOferta}")
-    @PreAuthorize("hasAuthority('Postulante')")
+    @PreAuthorize("hasAuthority('Reclutador')")
     public ResponseEntity<?> rankCandidatosPorOferta(@PathVariable Integer idOferta) {
         OfertaLaboral oferta = ofertaLaboralService.findById(idOferta);
         if (oferta == null) {
