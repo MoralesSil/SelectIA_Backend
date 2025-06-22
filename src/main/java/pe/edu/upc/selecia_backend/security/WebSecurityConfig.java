1bc07a74fbbc03814498ba9e06f40ca404ddfcb4
Daniel <danielhuertaarellanos.1.12@gmail.com>
2025-06-22 10:27:47 -0500

    avance

--- src/main/java/pe/edu/upc/selecia_backend/security/WebSecurityConfig.java
+++ src/main/java/pe/edu/upc/selecia_backend/security/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -70,7 +71,8 @@ public class WebSecurityConfig {
                         .requestMatchers(antMatcher("/user/id/{username}")).permitAll()
                         .requestMatchers(antMatcher("/usuario")).permitAll()
                         .requestMatchers(antMatcher("/registrarreclutador")).permitAll()
-                        .requestMatchers(antMatcher("/{id}")).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/ofertas-laborales").permitAll()
+                        .requestMatchers("/api/ofertas-laborales").authenticated()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
